c7a6724f990aa56b32c4c96210c8871ea66e9319
aminulrony1999 <aminulrony1999@gmail.com>
2024-07-18 19:36:39 +0600

    Linked list is created

--- Linked List/deletionLL.cpp
+++ Linked List/deletionLL.cpp
@@ -1,8 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 class Node
 {
-    public:
+public:
     int data;
     Node *next;
     Node(int data1)
@@ -11,7 +11,34 @@ class Node
         next = nullptr;
     }
 };
+
+Node *createLinkedList()
+{
+    int n;
+    cout << "Enter number of nodes in the linked list : ";
+    cin >> n;
+    cout << "Enter the nodes : ";
+    int x;
+    cin >> x;
+    Node *head = new Node(x);
+    Node *mover = head;
+    for (int i = 1; i < n; ++i)
+    {
+        cin >> x;
+        mover->next = new Node(x);
+        mover = mover->next;
+    }
+    return head;
+}
 int main()
 {
-
+    Node *head = createLinkedList();
+    Node *node = head;
+    cout << "Elements of the Linked List are : ";
+    while(node != nullptr)
+    {
+        cout<< node -> data<<" ";
+        node = node -> next;
+    }
+    return 0;
 }
