364f34dfab73536e0e0254a74cd4df30c4268008
aminulrony2024 <aminulrony2024@gmail.com>
2024-08-10 16:40:47 +0600

    push to stack is successfull

--- Stack/stackUsingLL.cpp
+++ Stack/stackUsingLL.cpp
@@ -16,33 +16,47 @@ public:
         data = data1;
         next = nullptr;
     }
-    void Push()
+    void Push(int x)
     {
+        Node *temp = top;
+        top = new Node(x);
+        top->next = temp;
     }
 };
 int main()
 {
     Node st;
-    cout<<"Enter the size of the stack : ";
+    cout << "Enter the size of the stack : ";
     int n;
-    cin>>n;
+    cin >> n;
     int x;
-    cin>>x;
+    cin >> x;
     st.top = new Node(x);
     Node *temp;
-    for(int i = 1; i < n; ++i)
+    for (int i = 1; i < n; ++i)
     {
-        cin>>x;
+        cin >> x;
         temp = st.top;
         st.top = new Node(x);
-        st.top -> next = temp;
+        st.top->next = temp;
     }
     temp = st.top;
-    cout<<"Element of the stack are : ";
-    while(temp != NULL)
+    cout << "Element of the stack are : ";
+    while (temp != NULL)
     {
-        cout<<temp -> data<<" ";
-        temp = temp -> next;
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl
+         << "Enter new element to be inserted in the stack :";
+    cin >> x;
+    st.Push(x);
+    cout << "Element of the stack after adding new element : ";
+    temp = st.top;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
     }
     return 0;
 }
