e6f1e0febda7380644e8fe2eb071a6ba48e2edf8
aminulrony2024 <aminulrony2024@gmail.com>
2024-08-14 13:58:29 +0600

    tree node enque is done in the queue

--- Tree/binaryTree.cpp
+++ Tree/binaryTree.cpp
@@ -1,64 +1,91 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node
+class Tnode
 {
-    public:
+public:
     int data;
-    Node *lchild;
-    Node *rchild;
-    Node(){}
-    Node(int data1)
+    Tnode *lchild;
+    Tnode *rchild;
+    Tnode() {}
+    Tnode(int data1)
     {
         data = data1;
         lchild = nullptr;
         rchild = nullptr;
     }
 };
+class Node
+{
+public:
+    Tnode *data;
+    Node *next;
+    Node()
+    {
+        next = nullptr;
+        data = nullptr;
+    }
+};
 class Queue
 {
-    public:
+public:
     Node *front;
     Node *rear;
     Queue()
     {
         front = rear = nullptr;
     }
-    void Enqueue()
-    {
-
-    }
-    void Dequeue()
-    {
-
-    }
     bool isFull()
     {
         Node *new_node = new Node;
-        if(new_node == NULL)
-        return true;
+        if (new_node == NULL)
+            return true;
         else
-        return false;
+            return false;
     }
     bool isEmpty()
     {
-        if(front == rear)
-        return true;
-        else 
-        return false;
+        if (front == NULL)
+            return true;
+        else
+            return false;
+    }
+    void Enqueue(Tnode *new_node)
+    {
+        Node *temp = nullptr;
+        temp->data = new_node;
+        temp->next = nullptr;
+        if (isFull())
+        {
+            cout << "Queue is full";
+        }
+        else
+        {
+            if (isEmpty())
+            {
+                front = rear = temp;
+            }
+            else
+            {
+                rear = temp;
+            }
+        }
+    }
+    void Dequeue()
+    {
     }
 };
 class Tree
 {
-    public:
-    Node *root = nullptr;
+public:
+    Tnode *root = nullptr;
     void createTree()
     {
-        Node *p = nullptr;
-        Node *t = nullptr;
+        Tnode *p = nullptr;
+        Tnode *t = nullptr;
         int x;
-        cout<<"Enter the root : ";
-        cin>>x;
-        root = new Node(x);
+        cout << "Enter the root : ";
+        cin >> x;
+        root = new Tnode(x);
         Queue Q;
     }
 };
@@ -66,4 +93,3 @@ int main()
 {
     return 0;
 }
-
