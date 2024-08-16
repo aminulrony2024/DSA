f2c3ef2b7328db23868e748b49d1751778fe5ca7
aminulrony2024 <aminulrony2024@gmail.com>
2024-08-16 10:24:25 +0600

    create binary tree and inorder bst success

--- Tree/bstUsingLL.cpp
+++ Tree/bstUsingLL.cpp
@@ -1,44 +1,85 @@
 #include <bits/stdc++.h>
 using namespace std;
-class Tnode
+class Node
 {
 public:
     int data;
-    Tnode *lchild;
-    Tnode *rchild;
-    Tnode(int data)
+    Node *lchild;
+    Node *rchild;
+    Node(int data)
     {
         this->data = data;
         lchild = rchild = nullptr;
     }
-    void Insert(Tnode *root, int key)
+};
+class BST
+{
+private:
+    Node *root;
+
+public:
+    BST()
     {
-        Tnode *r, *t, *p;
+        root = nullptr;
+    }
+    Node *getRoot()
+    {
+        return root;
+    }
+    void Insert(int key)
+    {
+        Node *r, *t, *p;
         t = root;
-        if(root == NULL)
+        if (root == nullptr)
         {
-            root = new Tnode(key);
+            root = new Node(key);
             return;
         }
-        while(t)
+        while (t != nullptr)
         {
             r = t;
-            if(t -> data == key)
-            return;
-            else if(t -> data > key)
-            t = t -> lchild;
+            if (key < t->data)
+            {
+                t = t->lchild;
+            }
+            else if (key > t->data)
+            {
+                t = t->rchild;
+            }
             else
-            t = t -> rchild;
+            {
+                return;
+            }
         }
-        p = new Tnode(key);
-        if(key < r -> data )
-        r -> rchild = p;
+        p = new Node(key);
+        if (key < r->data)
+            r->lchild = p;
         else
-        r -> lchild = p;
+            r->rchild = p;
+    }
+    void Inorder(Node *p)
+    {
+        if (p)
+        {
+            Inorder(p->lchild);
+            cout << p->data << " ";
+            Inorder(p->rchild);
+        }
     }
 };
 int main()
 {
-    Tnode *root = nullptr;
+    BST T;
+    cout << "Total nodes of the binary search tree : ";
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        int x;
+        cin >> x;
+        T.Insert(x);
+    }
+    cout << "In order Traversal of the binary search tree : ";
+    T.Inorder(T.getRoot());
     return 0;
 }
